8de0065d3fe7d3d36d1565ba7e7a698b889ef284
Jess Pring <jessicapring@outlook.com>
2021-02-08 16:09:22 +0000

    can get static data

--- app.js
+++ app.js
@@ -6,6 +6,25 @@ app.get('/', (req, res) => {
   res.send('Hello world!');
 })
 
+app.get('/products', (req, res) => {
+  const products = [
+    {
+      id: 1,
+      name: "pen"
+    },
+    {
+      id: 2,
+      name: "pencil"
+    },
+    {
+      id: 3,
+      name: "crayon"
+    }
+  ];
+
+  res.json(products);
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
